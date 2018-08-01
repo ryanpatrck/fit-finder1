4bc1b2e6f0f8a9e2a5210d75a3f9519f80c28269
ryanpatrck <ryanpatrck@gmail.com>
2018-08-01 15:52:48 -0400

    heroku

--- client/src/components/Publisher/Publisher.js
+++ client/src/components/Publisher/Publisher.js
@@ -31,12 +31,13 @@ class Publisher extends React.Component {
     return (
       <OTSession apiKey='46156272' sessionId="2_MX40NjE1NjI3Mn5-MTUzMjUyOTQ1MDMxMn4rNERmN1F4dkdseklKKys1cVAxejlqeDJ-UH4" token="T1==cGFydG5lcl9pZD00NjE1NjI3MiZzaWc9MThiZmJhZjEyOWFlYzMzZGQ0MGI1ZDIwMmMyNTRmYmM3ODQ3MzNmMzpzZXNzaW9uX2lkPTJfTVg0ME5qRTFOakkzTW41LU1UVXpNalV5T1RRMU1ETXhNbjRyTkVSbU4xRjRka2RzZWtsS0t5czFjVkF4ZWpscWVESi1VSDQmY3JlYXRlX3RpbWU9MTUzMjUyOTQ4MiZub25jZT0wLjk5Nzk1NTY1NzY5Nzk4Nzgmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTUzNTEyMTQ4MSZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==">
         
+       <OTSubscriber />
+        <OTStreams>
         <OTPublisher
         properties={this.publisherProperties}
         eventHandlers={this.publisherEventHandlers}
         />
-        <OTStreams>
-          <OTSubscriber />
+
         </OTStreams>
       </OTSession>
        

4a9ac0dfd563b76bbc7f4bff104b0269151c228c
ryanpatrck <ryanpatrck@gmail.com>
2018-08-01 16:03:10 -0400

    fix video

--- client/src/components/Publisher/Publisher.js
+++ client/src/components/Publisher/Publisher.js
@@ -4,19 +4,19 @@ import './Publisher.css'
 class Publisher extends React.Component {
   constructor(props) {
     super(props);
-
+ 
     this.subscriberProperties = {
       preferredFrameRate: 15,
       showControls: false,
-      width: 1000,
+      width: 1000, 
       height: 500,
     };
 
-    this.publisherProperties = {
-      width: 1000,
+    this.publisherProperties ={
+      width: 1000, 
       height: 500,
     }
-
+ 
     this.subscriberEventHandlers = {
       videoDisabled: event => {
         console.log('Subscriber video disabled!');
@@ -30,21 +30,21 @@ class Publisher extends React.Component {
     console.log(process);
     return (
       <OTSession apiKey='46156272' sessionId="2_MX40NjE1NjI3Mn5-MTUzMjUyOTQ1MDMxMn4rNERmN1F4dkdseklKKys1cVAxejlqeDJ-UH4" token="T1==cGFydG5lcl9pZD00NjE1NjI3MiZzaWc9MThiZmJhZjEyOWFlYzMzZGQ0MGI1ZDIwMmMyNTRmYmM3ODQ3MzNmMzpzZXNzaW9uX2lkPTJfTVg0ME5qRTFOakkzTW41LU1UVXpNalV5T1RRMU1ETXhNbjRyTkVSbU4xRjRka2RzZWtsS0t5czFjVkF4ZWpscWVESi1VSDQmY3JlYXRlX3RpbWU9MTUzMjUyOTQ4MiZub25jZT0wLjk5Nzk1NTY1NzY5Nzk4Nzgmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTUzNTEyMTQ4MSZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==">
-
-        <OTSubscriber
-         properties={this.subscriberProperties}
-         eventHandlers={this.subscriberEventHandlers}
+        
+        <OTPublisher
+        properties={this.publisherProperties}
+        eventHandlers={this.publisherEventHandlers}
         />
         <OTStreams>
-          <OTPublisher
-            properties={this.publisherProperties}
-            eventHandlers={this.publisherEventHandlers}
+          <OTSubscriber 
+          properties={this.subscriberProperties}
+          eventHandlers={this.subscriberEventHandlers}
           />
-
+          
         </OTStreams>
       </OTSession>
-
-
+       
+        
 
     );
   }
